9eec756ab5756519c3595b8a334d5d0c23044cc8
Andy Zhang <2279429006@qq.com>
2025-04-20 10:06:23 +0800

    Hw 12

---
+++ hw12/C.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <iomanip> 
+#include <cstring>
+using namespace std;
+template <class T>
+class CArray3D
+{
+// 在此处补充你的代码
+    T* data;
+    int d1, d2, d3;
+public:
+    CArray3D(int d1, int d2, int d3):d1(d1), d2(d2), d3(d3){
+        data = new T[d1*d2*d3];
+    }
+    class CProxy2D{
+        T* base;
+        int d2, d3;
+    public:
+        CProxy2D(T*base, int d2, int d3): base(base), d2(d2), d3(d3){}
+        T* operator[](int x){
+            return base+d3*x;
+        }
+        operator void*(){
+            return base;
+        }
+    };
+    CProxy2D operator[](int x){
+        return CProxy2D(data+x*d2*d3, d2, d3);
+    }
+};
+
+CArray3D<int> a(3,4,5);
+CArray3D<double> b(3,2,2);
+void PrintA()
+{
+	for(int i = 0;i < 3; ++i) {
+		cout << "layer " << i << ":" << endl;
+		for(int j = 0; j < 4; ++j) {
+			for(int k = 0; k < 5; ++k) 
+				cout << a[i][j][k] << "," ;
+			cout << endl;
+		}
+	}
+}
+void PrintB()
+{
+	for(int i = 0;i < 3; ++i) {
+		cout << "layer " << i << ":" << endl;
+		for(int j = 0; j < 2; ++j) {
+			for(int k = 0; k < 2; ++k) 
+				cout << b[i][j][k] << "," ;
+			cout << endl;
+		}
+	}
+}
+
+int main()
+{
+
+	int No = 0;
+	for( int i = 0; i < 3; ++ i ) {
+		a[i];
+		for( int j = 0; j < 4; ++j ) {
+			a[j][i];
+			for( int k = 0; k < 5; ++k )
+				a[i][j][k] = No ++;
+			a[j][i][i];	
+		}
+	}
+	PrintA();
+	memset(a[1],-1 ,20*sizeof(int));	
+	memset(a[1],-1 ,20*sizeof(int));
+	PrintA(); 
+	memset(a[1][1],0 ,5*sizeof(int));	
+	PrintA();
+
+	for( int i = 0; i < 3; ++ i )
+		for( int j = 0; j < 2; ++j )
+			for( int k = 0; k < 2; ++k )
+				b[i][j][k] = 10.0/(i+j+k+1);
+	PrintB();
+	int n = a[0][1][2];
+	double f = b[0][1][1];
+	cout << "****" << endl;
+	cout << n << "," << f << endl;
+		
+	return 0;
+}
